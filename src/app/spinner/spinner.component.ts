dd4df9f7a9a4a41d9c607bf9b561701668f97de3
Magda98 <magdakochman7@gmail.com>
2024-01-28 11:44:28 +0100

    add spinner

---
+++ src/app/spinner/spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  template: `<span class="loader"></span>`,
+  styleUrls: ['./spinner.component.css'],
+  standalone: true,
+})
+export class SpinnerComponent {}
