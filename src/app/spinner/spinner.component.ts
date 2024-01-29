f40bc9422ff3c1e7e95dd9831375f30b11334d63
Magda98 <magdakochman7@gmail.com>
2024-01-29 20:19:46 +0100

    update spinner

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-spinner',
   template: `<span class="loader"></span>`,
-  styleUrls: ['./spinner.component.css'],
+  styleUrls: ['./spinner.component.scss'],
   standalone: true,
 })
 export class SpinnerComponent {}
