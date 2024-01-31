6b12d2736affcad13cac1b9eab9a59f84b37f7e2
Magda98 <magdakochman7@gmail.com>
2024-01-31 16:05:18 +0100

    get albums

--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -19,3 +19,30 @@ export interface UserData {
 export interface Image {
   link: string;
 }
+
+export interface Album {
+  id: string;
+  title: string;
+  description: string | null;
+  datetime: number;
+  cover: string;
+  cover_edited: number;
+  cover_width: number;
+  cover_height: number;
+  account_url: string;
+  account_id: number;
+  privacy: string;
+  layout: string;
+  views: number;
+  link: string;
+  favorite: boolean;
+  nsfw: null;
+  section: null;
+  images_count: number;
+  in_gallery: boolean;
+  is_ad: boolean;
+  include_album_ads: boolean;
+  is_album: boolean;
+  deletehash: string;
+  order: number;
+}
--- src/app/shared/services/images.service.ts
+++ src/app/shared/services/images.service.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Signal, inject } from '@angular/core';
 import { ApiResponse } from '../interfaces/response';
-import { Image } from '../interfaces/interfaces';
+import { Album, Image } from '../interfaces/interfaces';
 import { createQuery } from '../utils/createQuery';
+import { UserService } from './user.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ImagesService {
   #http = inject(HttpClient);
+  #authService = inject(AuthService);
 
   getUserImages(page: Signal<number>) {
     return createQuery(
@@ -21,6 +24,15 @@ export class ImagesService {
     return createQuery(
       ['imagesCount'] as const,
       this.#http.get<ApiResponse<number>>(`/account/me/images/count`)
+    );
+  }
+
+  getAlbums(page: Signal<number>) {
+    return createQuery(
+      ['albums', { page: page() }] as const,
+      this.#http.get<ApiResponse<Album[]>>(
+        `account/${this.#authService.username}/albums/${page()}`
+      )
     );
   }
 }
