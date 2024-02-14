af6a6c4d9a8d01c581e1288f2117ef5a36f87b8c
Magdalena Bober <33430525+Magda98@users.noreply.github.com>
2024-02-14 07:39:40 +0000

    fix icon component

--- src/app/icon/icon.component.ts
+++ src/app/icon/icon.component.ts
@@ -1,16 +1,21 @@
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Icons, icons } from '../shared/utils/icons';
 import { Component, Input, OnChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+export type IconSize = 'small' | 'large' | 'normal'
 
 @Component({
   selector: 'mx-icon',
-  template: `<span class="icon" [outerHTML]="icon"></span>`,
+  template: `<span class="icon"  [outerHTML]="icon"></span>`,
   styleUrls: ['./icon.component.scss'],
-  host: { class: 'app-icon' },
+  host: { class: 'app-icon',  '[class]' : 'iconSize'  },
   standalone: true,
+  imports: [CommonModule]
 })
 export class IconComponent implements OnChanges {
   @Input() type!: Icons;
+  @Input() iconSize:  IconSize = 'normal';
   icon!: SafeHtml;
   constructor(private sanitizer: DomSanitizer) {}
 
